de464291142b69a13239b9cdaa1b758c338ed63e
Stefan Kapetanovic <stekap000@gmail.com>
2024-03-01 17:04:13 -0800

    Print function and panic function that forces jump to cpu reset vector

--- bootloader/bootloader.asm
+++ bootloader/bootloader.asm
@@ -1,13 +1,51 @@
+	org 0x7c00
 	use16
 
-	mov ah, 0Eh
-	mov al, 'T'
-	mov bx, 02h
-	int 10h
+	macro print str {
+		if ~ str eq si
+		push si
+		mov si, str
+		end if
+	
+		call _print
+	
+		if ~str eq si
+		pop si
+		end if
+	}
+
+	mov si, test_string
+	call _panic
 	
 black_hole:
 	jmp black_hole
 
+_panic:
+	print panic_msg
+	print si
+	xor ax, ax
+	int 16h
+	;; jump to cpu reset vector
+	;; because of real mode addresing, these two are equivalent
+	;; BIOS code is also mapped somewhere in the last segment, so this will also reload BIOS
+	;; which will then look for bootable disk and load our program at 0x7c00, and cycle continues
+	jmp far 0ffffh:0
+	;; jmp far 0f000h:0fff0h
+
+_print:
+	lodsb
+	or al, al
+	jz .end
+	mov ah, byte 0Eh
+	mov bx, word 03h
+	int 010h
+	jmp _print
+	.end:
+	ret
+
+test_string: db "lkjasd lkajsd", 0
+panic_msg:	 db "PANIC:", 0
+	
 dummy:  	db 510 - ($ - $$) dup 0
-boot_flag:	db 55h, 0AAh
+boot_mark:	db 55h, 0AAh
 	
